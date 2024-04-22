9949689fb33327d6b08d8c67b2c00c0a3645a8e0
Vinicius lucas <37543938+vinihoudini@users.noreply.github.com>
2024-04-22 20:43:58 -0300

    Add files via upload

---
+++ kanyew.js
@@ -0,0 +1,23 @@
+function generateQuote() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.kanye.rest', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var quote = JSON.parse(xhr.responseText).quote;
+            document.getElementById('quote').textContent = quote;
+        }
+    };
+    xhr.send();
+}
+
+function shareOnTwitter() {
+    var quote = document.getElementById('quote').textContent;
+    var siteUrl = 'https://vinihoudini.github.io'; 
+    var tweetText = '"' + quote + '"  Veja mais em: ' + siteUrl;
+    var tweetUrl = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweetText);
+    window.open(tweetUrl);
+}
+
+ src="kanyew.js"
+ src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+
